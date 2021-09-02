80f9dc898feec13505ef4a7c484f3ed88b904234
openopen114 <openopen114@gmail.com>
2021-09-02 09:58:37 +0800

    save

--- src/main/java/com/openopen/controller/GoogleDriveController.java
+++ src/main/java/com/openopen/controller/GoogleDriveController.java
@@ -1,8 +1,7 @@
 package com.openopen.controller;
 
 
-import com.openopen.googledrive.FileManager;
-import org.springframework.beans.factory.annotation.Value;
+import com.openopen.googledrive.GoogleDriveManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +14,12 @@ import java.io.IOException;
 public class GoogleDriveController {
 
 
-    @Value("${google.drive.folder.id}")
-    private String GOOGLE_DRIVE_FOLDER_ID;
-
-
     @RequestMapping(
             value = "/test",
             method = RequestMethod.GET)
     public String aaaa() throws IOException, InterruptedException {
-        System.out.println("==>GOOGLE_DRIVE_FOLDER_ID:" + GOOGLE_DRIVE_FOLDER_ID);
-        FileManager fileManager = new FileManager(GOOGLE_DRIVE_FOLDER_ID);
-        fileManager.uploadFile();
+        GoogleDriveManager googleDriveManager = new GoogleDriveManager();
+        googleDriveManager.uploadFile();
 
         return "OK";
     }
--- src/main/java/com/openopen/googledrive/GoogleDriveManager.java
+++ src/main/java/com/openopen/googledrive/GoogleDriveManager.java
@@ -3,11 +3,13 @@ package com.openopen.googledrive;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,6 +22,24 @@ import java.util.List;
 
 @Component
 public class GoogleDriveManager {
+
+
+    private static String uploadingFileMimeType = "image/jpeg";
+    private static String buildFolderPath = "/Users/openopen/Desktop/aaaa.jpeg";
+
+
+    // Google Drive Service
+    private Drive googleDriveService;
+
+
+    //要上傳的 folder id (要先共用給服務帳戶)
+    @Getter
+    private static String GOOGLE_DRIVE_FOLDER_ID;
+
+    @Value("${google.drive.folder.id}")
+    private void setGoogleDriveFolderId(String _googleDriveFolderId) {
+        GOOGLE_DRIVE_FOLDER_ID = _googleDriveFolderId;
+    }
 
 
     // 服務帳戶 email
@@ -93,6 +113,85 @@ public class GoogleDriveManager {
     public static Drive getGoogleDriveService() throws IOException {
         Credential credential = authorize();
         return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
+    }
+
+
+    /*
+     *
+     * 新增資料夾
+     *
+     *
+     * */
+    public File createFolder(String _parentId, String _folderName) throws IOException {
+
+        // 取得 Google Drive Service
+        this.googleDriveService = GoogleDriveManager.getGoogleDriveService();
+
+
+        //File MetaData
+        File folderMetadata = new File();
+
+
+        System.out.println("===> Folder Name: " + _folderName);
+        folderMetadata.setName(_folderName);
+        folderMetadata.setMimeType("application/vnd.google-apps.folder");
+
+        if (_parentId != null && _parentId.length() > 0) {
+            folderMetadata.setParents(Arrays.asList(_parentId));
+        }
+
+        //新增資料夾並回傳 id
+        return this.googleDriveService.files().create(folderMetadata).setFields("id").execute();
+    }
+
+
+    /*
+     *
+     * 新增檔案
+     *
+     *
+     * */
+    public File createFile(String _parentId, String _fileName, String _mimeType)
+            throws IOException {
+
+        // 取得 Google Drive Service
+        this.googleDriveService = GoogleDriveManager.getGoogleDriveService();
+
+
+        //File Metadata
+        File fileMetadata = new File();
+
+        fileMetadata.setName(_fileName);
+        fileMetadata.setMimeType(_mimeType);
+
+        if (_parentId != null && _parentId.length() > 0) {
+            fileMetadata.setParents(Arrays.asList(_parentId));
+        }
+
+        java.io.File localFile = new java.io.File(buildFolderPath);
+        FileContent mediaContent = new FileContent(_mimeType, localFile);
+
+        // 新增檔案 回傳 id
+        return this.googleDriveService.files().create(fileMetadata, mediaContent).setFields("id").execute();
+    }
+
+
+    /*
+     *
+     * 上傳檔案
+     *
+     * */
+    public void uploadFile() throws IOException, InterruptedException {
+//        File remoteFileFolder = this.createRemoteApkFolder(folderId);
+//        System.out.println("Remote Folder Id on Google Drive: " + remoteFileFolder.getId());
+
+//        Thread.sleep(2000);
+
+        String fileName = "aaaa123";
+
+
+        File remoteFile = this.createFile(GOOGLE_DRIVE_FOLDER_ID, fileName, uploadingFileMimeType);
+        System.out.println("Remote File Id on Google Drive: " + remoteFile.getId());
     }
 
 }
