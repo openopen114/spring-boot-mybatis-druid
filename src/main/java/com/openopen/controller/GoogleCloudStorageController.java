b4f3fa41919789bca184d76837d59ce9b0e4089f
openopen114 <openopen114@gmail.com>
2021-09-09 09:54:37 +0800

    save

--- src/main/java/com/openopen/controller/GoogleCloudStorageController.java
+++ src/main/java/com/openopen/controller/GoogleCloudStorageController.java
@@ -1,15 +1,20 @@
 package com.openopen.controller;
 
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.openopen.googlecloudstorage.GoogleCloudStorageAction;
+import net.sf.jmimemagic.MagicException;
+import net.sf.jmimemagic.MagicMatchNotFoundException;
+import net.sf.jmimemagic.MagicParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api/google/cloud/storage")
@@ -29,6 +34,46 @@ public class GoogleCloudStorageController {
         googleCloudStorageAction.createBucket(_id);
         logger.info("====> createBucket 1 ");
         return "OK 123";
+    }
+
+
+    /*
+     *
+     * 上傳圖片到 Google Cloud Storage
+     *
+     * */
+    //http://localhost:8080/api/google/cloud/storage/uploadObject
+    @PostMapping(
+            value = "/uploadObject",
+            produces = {"application/json"})
+    @Transactional
+    public String uploadObject(@RequestParam("file") MultipartFile fileInput,
+                               @RequestParam("file") MultipartFile fileMetaData,
+                               @RequestParam("json") String _json) throws IOException, InterruptedException, MagicMatchNotFoundException, MagicException, MagicParseException {
+        logger.info("===> uploadObject 0");
+        logger.info(_json);
+
+        InputStream fileInputStream = fileInput.getInputStream();
+
+
+//        Gson gson = new Gson();
+//        List<Person> list = gson.fromJson(_json, new TypeToken<List<Person>>() {
+//        }.getType());
+
+        GoogleCloudStorageAction googleCloudStorageAction = new GoogleCloudStorageAction();
+
+        googleCloudStorageAction.uploadObject("pub-001", fileInputStream, fileMetaData);
+
+        logger.info("===> uploadImage 1");
+        // String fileId = googleCloudStorageAction.uploadImage(fileInputStream, fileInputStream2, fileMetaData);
+
+
+        JsonObject obj = new JsonObject();
+        obj.addProperty("ACTION", "uploadImage");
+        obj.addProperty("RESULT", "OK");
+        //obj.addProperty("FILE_ID", fileId);
+
+        return new Gson().toJson(obj);
     }
 
 
--- src/main/java/com/openopen/googlecloudstorage/GoogleCloudStorageAction.java
+++ src/main/java/com/openopen/googlecloudstorage/GoogleCloudStorageAction.java
@@ -1,12 +1,15 @@
 package com.openopen.googlecloudstorage;
 
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.BucketInfo;
-import com.google.cloud.storage.Storage;
+import com.google.cloud.Identity;
+import com.google.cloud.Policy;
+import com.google.cloud.storage.*;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class GoogleCloudStorageAction {
 
@@ -15,14 +18,92 @@ public class GoogleCloudStorageAction {
     private static Logger logger = LoggerFactory.getLogger(GoogleCloudStorageAction.class);
 
 
-    public void createBucket(String _bucketName) throws IOException {
+    /*
+     *
+     * 建立新 Bucket
+     * https://cloud.google.com/storage/docs/creating-buckets#storage-create-bucket-code_samples
+     * */
+    public String createBucket(String _bucketName) throws IOException {
         logger.info("===> createBucket");
-        Storage googleCloudStorageService = GoogleCloudStorageAuth.getGoogleCloudStorageService();
-        // Creates the new bucket
-        Bucket bucket = googleCloudStorageService.create(BucketInfo.of(_bucketName));
 
-        logger.info("Bucket %s created.%n", bucket.getName());
-         
+        // 取得 google Cloud Storage Service
+        Storage googleCloudStorageService = GoogleCloudStorageAuth.getGoogleCloudStorageService();
+
+
+        // 儲存類型
+        StorageClass storageType = StorageClass.REGIONAL;
+        // 位置
+        String location = "ASIA-EAST1";
+
+
+        Bucket bucket =
+                googleCloudStorageService.create(
+                        BucketInfo.newBuilder(_bucketName)
+                                .setStorageClass(storageType)
+                                .setLocation(location)
+                                .build());
+
+
+        Policy originalPolicy = googleCloudStorageService.getIamPolicy(_bucketName);
+        googleCloudStorageService.setIamPolicy(
+                _bucketName,
+                originalPolicy
+                        .toBuilder()
+                        .addIdentity(StorageRoles.objectViewer(), Identity.allUsers()) // All users can view
+                        .build());
+
+        logger.info(
+                "Created bucket "
+                        + bucket.getName()
+                        + " in "
+                        + bucket.getLocation()
+                        + " with storage class "
+                        + bucket.getStorageClass());
+
+
+        return bucket.getName();
+    }
+
+
+    public static void uploadObject(
+            String bucketName, InputStream _fileInputStream,
+            MultipartFile _fileMetaData) throws IOException {
+
+        // 取得 google Cloud Storage Service
+        Storage googleCloudStorageService = GoogleCloudStorageAuth.getGoogleCloudStorageService();
+
+
+        // The ID of your GCP project
+        // String projectId = "your-project-id";
+
+        // The ID of your GCS bucket
+        // String bucketName = "your-unique-bucket-name";
+
+        // The ID of your GCS object
+        // String objectName = "your-object-name";
+
+        // The path to your file to upload
+        // String filePath = "path/to/your/file"
+
+
+        /* ◢◤◢◤◢◤◢◤◢◤ 2. 取得檔案名稱, mime type ◢◤◢◤◢◤◢◤◢◤ */
+        logger.info("2. 取得檔案名稱, mime type 0");
+
+        // 完整檔案名稱 xxx.yyy
+        String fullFileName = _fileMetaData.getOriginalFilename();
+
+        String fileType = fullFileName.substring(fullFileName.lastIndexOf(".") + 1, fullFileName.length());
+
+
+        BlobId blobId = BlobId.of(bucketName, fullFileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+        googleCloudStorageService.create(blobInfo, IOUtils.toByteArray(_fileInputStream));
+
+
+        logger.info("blobId getMediaLink :" + blobInfo.getMediaLink());
+        logger.info("blobId getName :" + blobId.getName());
+
+
     }
 
 }
