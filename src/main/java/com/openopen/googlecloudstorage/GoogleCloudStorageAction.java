f232f22ed02851ad6522330327434d597b14b4a8
openopen114 <openopen114@gmail.com>
2021-09-09 10:38:30 +0800

    google cloud storage upload

--- src/main/java/com/openopen/googlecloudstorage/GoogleCloudStorageAction.java
+++ src/main/java/com/openopen/googlecloudstorage/GoogleCloudStorageAction.java
@@ -4,6 +4,8 @@ import com.google.cloud.Identity;
 import com.google.cloud.Policy;
 import com.google.cloud.storage.*;
 import org.apache.commons.io.IOUtils;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
@@ -69,40 +71,46 @@ public class GoogleCloudStorageAction {
             String bucketName, InputStream _fileInputStream,
             MultipartFile _fileMetaData) throws IOException {
 
-        // 取得 google Cloud Storage Service
+        /* ◢◤◢◤◢◤◢◤◢◤ 1. 取得 google Cloud Storage Service ◢◤◢◤◢◤◢◤◢◤ */
         Storage googleCloudStorageService = GoogleCloudStorageAuth.getGoogleCloudStorageService();
 
 
-        // The ID of your GCP project
-        // String projectId = "your-project-id";
-
-        // The ID of your GCS bucket
-        // String bucketName = "your-unique-bucket-name";
-
-        // The ID of your GCS object
-        // String objectName = "your-object-name";
-
-        // The path to your file to upload
-        // String filePath = "path/to/your/file"
-
-
-        /* ◢◤◢◤◢◤◢◤◢◤ 2. 取得檔案名稱, mime type ◢◤◢◤◢◤◢◤◢◤ */
-        logger.info("2. 取得檔案名稱, mime type 0");
+        /* ◢◤◢◤◢◤◢◤◢◤ 2. 取得檔案名稱  ◢◤◢◤◢◤◢◤◢◤ */
+        logger.info("2. 取得檔案名稱");
 
         // 完整檔案名稱 xxx.yyy
-        String fullFileName = _fileMetaData.getOriginalFilename();
 
+
+        DateTimeZone timeZone = DateTimeZone.forID("Asia/Taipei");
+        DateTime dateTime = new DateTime(timeZone);
+        String timestamp = dateTime.toString("yyyy-MM-dd_HH:mm:ss");
+
+
+        String fullFileName = _fileMetaData.getOriginalFilename();
+        assert fullFileName != null;
+        String fileName = fullFileName.substring(0, fullFileName.lastIndexOf("."));
         String fileType = fullFileName.substring(fullFileName.lastIndexOf(".") + 1, fullFileName.length());
 
 
-        BlobId blobId = BlobId.of(bucketName, fullFileName);
+        String fullFileNameWithTimestamp = fileName + "_" + timestamp + "." + fileType;
+
+        logger.info("==> fullFileNameWithTimestamp:" + fullFileNameWithTimestamp);
+
+
+
+        /* ◢◤◢◤◢◤◢◤◢◤ 3. 上傳 Blob  ◢◤◢◤◢◤◢◤◢◤ */
+
+        BlobId blobId = BlobId.of(bucketName, fullFileNameWithTimestamp);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
         googleCloudStorageService.create(blobInfo, IOUtils.toByteArray(_fileInputStream));
 
 
-        logger.info("blobId getMediaLink :" + blobInfo.getMediaLink());
-        logger.info("blobId getName :" + blobId.getName());
+        logger.info("file name " + fullFileNameWithTimestamp);
+        logger.info("URLEncoder file name " + java.net.URLEncoder.encode(fullFileNameWithTimestamp, "UTF-8"));
+        // URL: https://storage.googleapis.com/[BUCKET NAME]/[URL ENCODE FILE NAME]
+        //java.net.URLEncoder.encode
 
+        logger.info("upload done");
 
     }
 
